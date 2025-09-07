f5f7ab7d53ce51cb5a04e7c467ef0eb0c0553801
Saranga Pani Dutta <duttasarangapani0@gmail.com>
2025-09-07 08:07:36 +0000

    And this button feels out of place so keep is same as the Three Lined mo

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -4,14 +4,11 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { VariantProps, cva } from "class-variance-authority"
-import { PanelLeft } from "lucide-react"
+import { Menu } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Separator } from "@/components/ui/separator"
 import { Sheet, SheetContent, SheetTitle } from "@/components/ui/sheet"
-import { Skeleton } from "@/components/ui/skeleton"
 import {
   Tooltip,
   TooltipContent,
@@ -164,7 +161,7 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      <PanelLeft />
+      <Menu />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
