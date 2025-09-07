df9b9c56d96c3f0a5b69c917cc8d4a10a5f04fea
Saranga Pani Dutta <duttasarangapani0@gmail.com>
2025-09-07 05:07:54 +0000

    Why did the camera orientation become so small? (_for element <FaceScann

--- src/app/scanner/page.tsx
+++ src/app/scanner/page.tsx
@@ -8,7 +8,7 @@ import MainLayout from '@/components/main-layout';
 export default function ScannerPage() {
   return (
     <MainLayout title="Take Attendance">
-      <div className="flex-1 w-full max-w-4xl mx-auto px-4 sm:px-8 pb-4 sm:pb-8 flex flex-col">
+      <div className="flex-1 flex flex-col w-full max-w-4xl mx-auto pb-4 sm:pb-8">
         <Card className="overflow-hidden shadow-2xl flex-1">
           <CardContent className="p-0 h-full">
             <FaceScanner />
